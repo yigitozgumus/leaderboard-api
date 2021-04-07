f24fc7520c35d383e459aeddd6a34d58685fb542
Yigit Ozgumus <yigitozgumus1@gmail.com>
2021-04-07 23:16:16 +0300

    Add router to the server and handler method declarations for leaderboard endpoint

--- server/server.go
+++ server/server.go
@@ -1,11 +1,12 @@
 package server
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
 
-const prefix = "/leaderboard"
+const jsonContentType = "application/json"
 
 type User struct {
 	displayName string
@@ -15,19 +16,56 @@ type User struct {
 }
 
 type UserStore interface {
-	getUserRankings() []User
+	GetUserRankings() []User
+	GetUserRankingsFiltered(country string) []User
 }
 
 type LeaderboardServer struct {
 	store UserStore
+	http.Handler
 }
 
-func (l *LeaderboardServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func NewLeaderboardServer(store UserStore) *LeaderboardServer {
+	l := new(LeaderboardServer)
+	l.store = store
+	router := http.NewServeMux()
 
-	if r.URL.Path != prefix {
-		w.WriteHeader(http.StatusNotFound)
+	router.Handle("/leaderboard", http.HandlerFunc(l.leaderboardHandler))
+	router.Handle("/leaderboard/", http.HandlerFunc(l.leaderboardFilterHandler))
+	router.Handle("/leaderboard/score/submit", http.HandlerFunc(l.scoreSubmissionHandler))
+	router.Handle("/leaderboard/user/profile/", http.HandlerFunc(l.userProfileHandler))
+	router.Handle("/leaderboard/user/create", http.HandlerFunc(l.createUserHandler))
+	l.Handler = router
+
+	return l
+}
+
+func (l *LeaderboardServer) leaderboardHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("content-type", jsonContentType)
+	json.NewEncoder(w).Encode(l.store.GetUserRankings())
+	w.WriteHeader(http.StatusOK)
+}
+
+func (l *LeaderboardServer) leaderboardFilterHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("content-type", jsonContentType)
+	country := r.URL.Path[len("/leaderboard/"):]
+	if len(country) == 0 {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprint(w, "country code must not be null")
 		return
 	}
+	json.NewEncoder(w).Encode(l.store.GetUserRankingsFiltered(country))
 	w.WriteHeader(http.StatusOK)
-	fmt.Fprint(w, l.store.getUserRankings())
+}
+
+func (l *LeaderboardServer) scoreSubmissionHandler(w http.ResponseWriter, r *http.Request) {
+	// FIXME
+}
+
+func (l *LeaderboardServer) userProfileHandler(w http.ResponseWriter, r *http.Request) {
+	// FIXME
+}
+
+func (l *LeaderboardServer) createUserHandler(w http.ResponseWriter, r *http.Request) {
+	// FIXME
 }
